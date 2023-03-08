34e89f7e90fe3091e36bd848d83044b22d3036b4
Edwin Hadass <EDWIN.HADASS@hotmail.com>
2023-03-08 12:26:43 +0000

    Adds Recursion and User Input

--- AND Gate and Linker Sequence Generator/AND_Gate_and_Linker_Sequence_Generator.py
+++ AND Gate and Linker Sequence Generator/AND_Gate_and_Linker_Sequence_Generator.py
@@ -1,1 +1,69 @@
+import string
 
+miRNADict = {}
+
+#dictionary defining complimentary base pairing (no wobble pairing present)
+baseDict = {"A":"U", "C":"G"}
+
+#list of and regions
+andRegions = ("AAAAAA", "UUUUUU", "CCCCCC", "GGGGGG")
+
+#computes complimentary strand 
+def compStrand(rnaStrand):
+    compliment = ""
+    for i in rnaStrand:
+        for key, value in baseDict.items():
+            if i == key:
+                compliment += value
+            elif i == value:
+                compliment += key
+    return compliment
+
+#reverses string
+def reverse(rnaStrand):
+  return rnaStrand[::-1]
+
+
+#computes and gate
+def andGateGen(sequenceDict, startIndex):#Maybe the way we do it recursively is by asking for a start index as an input, then replace the indexes of the dictionary with index, index+1
+    endIndex = len(sequenceDict)
+    if(startIndex == endIndex -1):
+        print("Done")
+    else:
+        #splitting each miRNA
+        miRNA1, miRNA2 = compStrand(list(sequenceDict.values())[startIndex] [:len(list(sequenceDict.values())[startIndex+1])//3]), compStrand(list(sequenceDict.values())[1] [:len(list(sequenceDict.values())[1])//3])
+        print("length of spliced Strand1: ", len(miRNA1))
+        print("length of spliced Strand2: ", len(miRNA2))
+
+        #index is incrimented later to update the andRegion such that its adjacent base doesn't match itself
+        index = 0
+
+        #this sets the default value of the andRegion to AAAAAA
+        andRegion = andRegions[index] 
+
+        #checking to see if the andRegion borders an identical base and changing it to correct this by incrimenting the index
+        #this only needs to be done twice as in the "worst case scenario", ALL of the below code will run, making the anndRegion different to BOTH of its neighboring bases
+        if (miRNA1[-1] == andRegion[1]):
+            index += 1
+            andRegion = andRegions[index] 
+            if (miRNA2[0] == andRegion[1]):
+                index += 1
+                andRegion = andRegions[index]
+        print(miRNA1 + andRegion + miRNA2)
+        andGateGen(miRNADict, startIndex+ 1)
+        return((miRNA1 + andRegion + miRNA2))
+    #Edwin maybe from here you could do something with recursion to call this function on (Strand[N], strand[N+1]) to output however many and gates we need for a given generation
+
+#Handles user input
+def start():
+    print("Input the number of miRNA strands")
+    num_miRNA = int(input())
+    i = 0
+    while(i < num_miRNA):
+        print("input the miRNA and its sequence")
+        miRNA = input()#User input must be in the same form as this: name,base sequence. Base sequence needs to be in all caps
+        index = miRNA.split(",")
+        miRNADict.update({index[0]: index[1]})
+        i+=1
+    andGateGen(miRNADict, 0)
+start()
