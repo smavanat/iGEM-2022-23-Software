3758243eb0f126c8ce4713ae579dc03a3b0fa507
Edwin Hadass <EDWIN.HADASS@hotmail.com>
2023-03-18 15:46:13 +0000

    Added a complex generator as well as direct inputs into the Nupack test tube system

--- AND Gate and Linker Sequence Generator/AND_Gate_and_Linker_Sequence_Generator.py
+++ AND Gate and Linker Sequence Generator/AND_Gate_and_Linker_Sequence_Generator.py
@@ -1,9 +1,13 @@
-import json
 import string
-import textwrap
+import math as m
+from nupack import *
+import matplotlib.pyplot as plt
+
+thsModel = Model(material='rna', celsius=37)
 
 #File for storing output
-outputFile = open("miRNA.txt", "w")
+outputFile = open("triggerComplex.txt", "w")
+outputFile2 = open("toeholdSwitch.txt", "w")
 
 #Dictionary for storing user input
 miRNADict = {}
@@ -16,6 +20,18 @@ baseList = ["A", "U", "C", "G"]
 
 #list of and regions
 andRegions = ("AAAAAA", "UUUUUU", "CCCCCC", "GGGGGG")
+
+#complex structure in DU+
+complexStructure = []
+
+#strands to be put together in the file
+strands = []
+
+#list of domain names for NUPACK
+domains = list((string.ascii_lowercase))
+
+#list of complex.seq
+seq = []
 
 #computes complimentary strand 
 def compStrand(rnaStrand):
@@ -32,57 +48,81 @@ def compStrand(rnaStrand):
 def reverse(rnaStrand):
   return rnaStrand[::-1]
 
+#splits each miRNAInput into thirds and adds those to the storageArray
 def splitString(miRNAInput):
     storageArray = []
     miRNAInput = miRNAInput.replace(" ","")
-    n = len(miRNAInput)//3
+    n = round(len(miRNAInput)//3)
     part1 = miRNAInput[:n]
-    part2 = miRNAInput[2*n:]
+    part2 = miRNAInput[2*n+1:]
+    part3 = miRNAInput[n:2*n+1]#modified
     storageArray.append(compStrand(part1))
     storageArray.append(compStrand(part2))
+    storageArray.append(part3)#Modified
     return storageArray
 
 #computes and gate
-def andGateGen(sequenceDict):#The way we do it recursively is by asking for a start index as an input, then replace the indexes of the dictionary with index, index+1
-    #endIndex = len(sequenceDict)
+def andGateGen(sequenceDict):
     storageDict = sequenceDict.values()
     storageArray = []
     miRNAPartArray = []
-
+    global unpairedMiddleArray
+    unpairedMiddleArray = []#modified
+ 
     for element in storageDict:
         storageArray = splitString(element)
         miRNAPartArray.append(storageArray[0])
         miRNAPartArray.append(storageArray[1])
+        unpairedMiddleArray.append(storageArray[2])#modified
 
+    #makes sure code runs on the end of 1 strand and the beginning of the other
     del miRNAPartArray[0]
     del miRNAPartArray[len(miRNAPartArray)-1]
+    middleIndex = 0
+    for element in miRNAPartArray:
 
-    for element in miRNAPartArray: 
         if(miRNAPartArray.index(element) % 2==0):
-
             #index is incrimented later to update the andRegion such that its adjacent base doesn't match itself
             index = 0
 
             #this sets the default value of the andRegion to AAAAAA
             andRegion = andRegions[index]
 
-            miRNA1 = miRNAPartArray[miRNAPartArray.index(element)]
+            global miRNA1, miRNA2
+
+            miRNA1 = reverse(miRNAPartArray[miRNAPartArray.index(element)])
+
             miRNA2 = miRNAPartArray[miRNAPartArray.index(element) + 1]
+
             #checking to see if the andRegion borders an identical base and changing it to correct this by incrimenting the index
             #this only needs to be done twice as in the "worst case scenario", ALL of the below code will run, making the anndRegion different to BOTH of its neighboring bases
             if (miRNA1[-1] == andRegion[1]):
                 index += 1
-            andRegion = andRegions[index] 
-            if (miRNA2[0] == andRegion[1]):
+                andRegion = andRegions[index]
+            if (reverse(miRNA2)[0] == andRegion[1]):
                 index += 1
                 andRegion = andRegions[index]
-            print(miRNA1 + andRegion + miRNA2)
+            if (reverse(miRNA2)[0] and miRNA1[-1] == andRegion[1]):
+                index += 1
+                andRegion = andRegions[index]            
+
+            #defining a repeating unit for the complex
+            duplex1 = "D" + str(len(miRNA1)) + "+"
+            complexAnd = "U" + str(len(andRegion))
+            duplex2 = "D" + str(len(miRNA2)) + "+"
+            unpairedMiddle = "U" + str(len(unpairedMiddleArray[middleIndex]))#modified
+
+            repeatUnit = [duplex1, complexAnd, duplex2, unpairedMiddle]       
+            andGate = miRNA1 + andRegion + reverse(miRNA2)
+
+            strands.append(andGate)
+            middleIndex+=1#modified
+
+            #adding repeat unit to an empty array
+            complexStructure.extend(repeatUnit)
         else:
             continue
-    
     print("Done")
-#        return((miRNA1 + andRegion + miRNA2))
-
 
 #Ensures correct inputs
 def checkCorrectCharacters(characterList, userInput:string):
@@ -93,7 +133,6 @@ def checkCorrectCharacters(characterList, userInput:string):
         else: 
             return True
     return False
-
 
 #Handles user input
 def start():
@@ -109,6 +148,131 @@ def start():
             i+=1
         else:
             print("Incorrect input")
-    json.dump(miRNADict, outputFile)
     andGateGen(miRNADict)
 start()
+
+unpairedStartLength = len(list(miRNADict.values())[0]) - round((len(list(miRNADict.values())[0])//3))
+
+if (unpairedStartLength % 3 == 2):
+    unpairedStartLength = unpairedStartLength + 1 
+elif (unpairedStartLength % 3 == 1):
+    unpairedStartLength = unpairedStartLength - 1
+else:
+    unpairedStartLength = unpairedStartLength
+
+unpairedStart = "U" + str(unpairedStartLength)
+unpairedEnd = "U" + str(len(list(miRNADict.values())[-1]) - len(miRNA2))
+complexStructure[-1] = unpairedEnd
+complexStructure.insert(0, unpairedStart)
+
+for value in miRNADict.values():
+    strands.append(value)
+
+domainCodes = []
+
+for strand, domain in zip(strands, domains):
+    domainCode = "domain " + domain + " = " + strand
+    domainCodes.append(domainCode)
+
+domainsUsed = domains[0:len(domainCodes)]
+
+mid = int((len(domainsUsed) + 1)/2)
+
+miRNAs = list(domainsUsed[mid-1:])
+andGates = list(domainsUsed[0:mid-1])
+
+seq.extend(miRNAs)
+
+integer = 0
+
+while (integer < len(andGates)):
+    for domain in seq:
+        if((seq.index(domain)%2 ==0) and (seq.index(domain) + 1 < len(seq))):
+            seq.insert(seq.index(domain) + 1, andGates[integer])
+            integer += 1
+        else:
+            continue
+
+outputFile.write(f'# miRNA complex structure in DU+ notation \nstructure complex = {"".join(complexStructure)}\n')
+outputFile.write(f'\n# base sequences for each RNA strand')
+for code in domainCodes:
+    outputFile.write(f"\n" + code)
+outputFile.write(f'\n \n# mapping RNA strands to the complex')
+outputFile.write(f'\ncomplex.seq = ')
+for sequence in seq:
+    outputFile.write(sequence + " ")
+
+#toehold switch stuff
+toeholdStart = list(miRNADict.values())[0][0:unpairedStartLength]
+toeholdEnd = list(miRNADict.values())[-1][len(miRNA2):]
+toeholdAndGates = []
+
+middleStrands = strands[0:-1]
+del middleStrands[len(middleStrands)//2]
+
+for loopStrand in middleStrands:
+    count = 1
+    for char in range(0, len(loopStrand)):
+        if (loopStrand[char - 1] == loopStrand[char]):
+            count+=1
+            if (count == 6):
+                toeholdAndGates.append(count*loopStrand[char])
+            else:
+                continue
+        else:
+            count = 1
+
+del unpairedMiddleArray[0]
+del unpairedMiddleArray[-1]
+ 
+integer = 0
+
+while (integer < len(unpairedMiddleArray)):
+    for domain in toeholdAndGates:
+        if((toeholdAndGates.index(domain)%2 ==0) and (toeholdAndGates.index(domain) + 1 < len(toeholdAndGates))):
+            toeholdAndGates.insert(toeholdAndGates.index(domain) + 1, unpairedMiddleArray[integer])
+            integer += 1
+        else:
+            continue
+
+toeholdRegion = []
+
+toeholdRegion.insert(0, toeholdStart)
+toeholdRegion.extend(toeholdAndGates)
+toeholdRegion.append(toeholdEnd)
+toeholdRegion = "".join(str(i) for i in toeholdRegion)
+
+toehold = round((len(list(miRNADict.values())[0])//2))
+thsDuplex = len(toeholdRegion) - toehold
+
+thsStructure = ("U"+ str(toehold) + "D"+ str(thsDuplex) + "(U3D5(U15)U3)U21")
+
+toeholdSwitchSequence = (compStrand(toeholdRegion) + "GGAUUUGCAAAAAAAAGAGGAGAGUAAAAUG" + reverse(toeholdRegion)[0:thsDuplex] + "AACCUGGCGGCAGCGCAAAAG")
+
+outputFile2.write(f'# toehold switch structure in DU+ notation \n')
+outputFile2.write(f'structure toeholdSwitch = ' + thsStructure + '\n')
+outputFile2.write(f'\n# base sequences for each section of the toehold switch \n')
+outputFile2.write(f'domain a = ' + compStrand(toeholdRegion) + '\n')
+outputFile2.write(f'domain b = GGAUUUGCAAAAAAAAGAGGAGA N5 AUG \n')
+outputFile2.write(f'domain c = ' + reverse(toeholdRegion)[0:thsDuplex] + '\n')
+outputFile2.write(f'domain d = AACCUGGCGGCAGCGCAAAAG \n')
+outputFile2.write(f'# toeholdSwitch sequence = ' + toeholdSwitchSequence + '\n')
+outputFile2.write(f'\n# mapping RNA domains to the toehold switch \n')
+outputFile2.write(f'toeholdSwitch.seq = a b c d')
+
+strands.append(toeholdSwitchSequence)
+domainsUsed.append(domains[len(domainsUsed)])
+
+attributeDict = {}
+strandDict = {}
+
+# strand_n = Strand("ACGUAGCUAGCUAGCAUC", name="n")
+# strand_n = Strand(Sequence, name="")
+
+for strand, letter in zip(strands, domainsUsed):
+    attributeDict[strand] = letter 
+    name = letter 
+    strandDict[Strand(strand, name = name)] = 5e-6
+    
+t1 = Tube(strands = strandDict, complexes = SetSpec(max_size = len(strandDict)), name="t1")
+tube_result = tube_analysis(tubes=[t1], compute=['mfe'], model=thsModel)
