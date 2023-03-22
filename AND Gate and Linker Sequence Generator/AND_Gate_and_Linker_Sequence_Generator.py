eb6d7641a9dd904fcac7cd50b36ce567cc7f86f3
Edwin Hadass <EDWIN.HADASS@hotmail.com>
2023-03-22 20:38:50 +0000

    The code now works for only one miRNA and is a little bit tidier

--- AND Gate and Linker Sequence Generator/AND_Gate_and_Linker_Sequence_Generator.py
+++ AND Gate and Linker Sequence Generator/AND_Gate_and_Linker_Sequence_Generator.py
@@ -136,175 +136,204 @@ def checkCorrectCharacters(characterList, userInput:string):
             return True
     return False
 
+#gets the length of the unpaired start and end.
+def unpairedParts(numMiRNA):
+    global unpairedStartLength 
+    unpairedStartLength = len(list(miRNADict.values())[0]) - round((len(list(miRNADict.values())[0])//3))
+
+    #this was the annoying thing I was talking about ages ago where due to divisibility by 3 resulting in a number that may round up or down depending on the remainder. fixed by brute force
+    if (unpairedStartLength % 3 == 2):
+        unpairedStartLength = unpairedStartLength + 1 
+    elif (unpairedStartLength % 3 == 1):
+        unpairedStartLength = unpairedStartLength - 1
+    else:
+        unpairedStartLength = unpairedStartLength
+
+    #converting unpairedStart and End to DU+ notation and adding to the beginning and end of complexStructure respectively
+    if(numMiRNA == 1):
+        unpairedStart = "U" + str(unpairedStartLength)
+        complexStructure.insert(0, unpairedStart)
+    else:
+        unpairedStart = "U" + str(unpairedStartLength)
+        unpairedEnd = "U" + str(len(list(miRNADict.values())[-1]) - len(miRNA2))
+        complexStructure[-1] = unpairedEnd
+        complexStructure.insert(0, unpairedStart)
+
+#Combines two lists together into one array so that their constituent variables appear in an alternating sequence
+def listAddition(array1, array2):
+    integer = 0
+    while (integer < len(array1)):
+        for domain in array2:
+            if((array2.index(domain)%2 ==0) and (array2.index(domain) + 1 < len(array2))):
+                array2.insert(array2.index(domain) + 1, array1[integer])
+                integer += 1
+            else:
+                continue
+
+def domainOutputs():
+    domainCodes = []
+
+    #this writes the text to be put in the txt file for assigning domains to the DU+ structure, eg
+    #domain a = AUCGACGACUGCCGUA
+    #domain b = UUCGUAGCUAGCUGA etc...
+    for strand, domain in zip(strands, domains):
+        domainCode = "domain " + domain + " = " + strand
+        domainCodes.append(domainCode)
+
+    #this takes letters of the alphabet in order until there is 1 for each miRNA and AND gate
+    global domainsUsed
+    domainsUsed = domains[0:len(domainCodes)]
+
+    #the domainsUsed list is split into 2 halves, one with letters to be assigned to miRNAs and the other to AND gates
+    mid = int((len(domainsUsed) + 1)/2)
+    miRNAs = list(domainsUsed[mid-1:])
+    andGates = list(domainsUsed[0:mid-1])
+
+    seq.extend(miRNAs)
+
+    #reordering the miRNAs and AND gates to be in a pattern of: miRNA, AND gate, miRNA, AND gate, miRNA etc...
+    listAddition(andGates, seq)
+
+    #writes all the calculated info to a text file
+    outputFile.write(f'# miRNA complex structure in DU+ notation \nstructure complex = {"".join(complexStructure)}\n')
+    outputFile.write(f'\n# base sequences for each RNA strand')
+    for code in domainCodes:
+        outputFile.write(f"\n" + code)
+    outputFile.write(f'\n \n# mapping RNA strands to the complex')
+    outputFile.write(f'\ncomplex.seq = ')
+    for sequence in seq:
+        outputFile.write(sequence + " ")
+
+def toeholdSwitchGen(numMiRNA):
+    #toehold switch stuff
+    #assigning start and end of toeholdRegion
+    if(numMiRNA !=1):
+        toeholdStart = list(miRNADict.values())[0][0:unpairedStartLength]
+        toeholdEnd = list(miRNADict.values())[-1][len(miRNA2):]
+
+        toeholdAndGates = []
+
+        #every strand but the first and last one which are already accounted for in toeholdStart and End
+        middleStrands = strands[0:-1]
+        del middleStrands[len(middleStrands)//2]
+
+        #checks the rest of the strands to see if they are and gates
+        for loopStrand in middleStrands:
+            count = 1
+            for char in range(0, len(loopStrand)):
+                if (loopStrand[char - 1] == loopStrand[char]):
+                    count+=1
+                    if (count == 6):
+                        toeholdAndGates.append(count*loopStrand[char])
+                    else:
+                        continue
+                else:
+                    count = 1
+
+        del unpairedMiddleArray[0]
+        del unpairedMiddleArray[-1]
+
+        #reordering sequences to form the toehold region
+        listAddition(unpairedMiddleArray, toeholdAndGates)
+
+        toeholdRegion = []
+
+        #forming the toehold region
+        toeholdRegion.insert(0, toeholdStart)
+        toeholdRegion.extend(toeholdAndGates)
+        toeholdRegion.append(toeholdEnd)
+    else:
+        toeholdStart = list(miRNADict.values())[0][0:unpairedStartLength]
+        toeholdRegion = []
+        toeholdRegion.insert(0, toeholdStart)
+    toeholdRegion = "".join(str(i) for i in toeholdRegion)
+
+    #this is the unpaired region of the toehold switch where the miRNA complex binds
+    toehold = round((len(list(miRNADict.values())[0])//2))
+
+    #the paired section of the toehold switch that unzips until the start codom
+    thsDuplex = len(toeholdRegion) - toehold
+
+    #the entire switch structure in DU+ including the hairpin loop
+    thsStructure = ("U"+ str(toehold) + "D"+ str(thsDuplex) + "(U3D5(U15)U3)U21")
+
+    #sequence of the toehold switch
+    toeholdSwitchSequence = (compStrand(toeholdRegion) + "GGAUUUGCAAAAAAAAGAGGAGAGUAAAAUG" + reverse(toeholdRegion)[0:thsDuplex] + "AACCUGGCGGCAGCGCAAAAG")
+
+    #writing the text file for ths
+    outputFile2.write(f'# toehold switch structure in DU+ notation \n')
+    outputFile2.write(f'structure toeholdSwitch = ' + thsStructure + '\n')
+    outputFile2.write(f'\n# base sequences for each section of the toehold switch \n')
+    outputFile2.write(f'domain a = ' + compStrand(toeholdRegion) + '\n')
+    outputFile2.write(f'domain b = GGAUUUGCAAAAAAAAGAGGAGA N5 AUG \n')
+    outputFile2.write(f'domain c = ' + reverse(toeholdRegion)[0:thsDuplex] + '\n')
+    outputFile2.write(f'domain d = AACCUGGCGGCAGCGCAAAAG \n')
+    outputFile2.write(f'# toeholdSwitch sequence = ' + toeholdSwitchSequence + '\n')
+    outputFile2.write(f'\n# mapping RNA domains to the toehold switch \n')
+    outputFile2.write(f'toeholdSwitch.seq = a b c d')
+
+    #creating separate Strand object for toehold switch
+    thsObject = Strand(toeholdSwitchSequence, domains[len(domainsUsed)])
+
+def tubeDef(numMiRNA):
+    #attribute dict is a dictionary pairing each strand to a corresponding letter, these do not need to be the same letters used in the text files as these are for analysing the MFE of complexes
+    attributeDict = {}
+
+    #strand dict is the dictionary pairing each strand object to a corresponding concentration in M to then be passed onto a tube
+    strandDict = {}
+
+    #note to self:
+    # strand_n = Strand("ACGUAGCUAGCUAGCAUC", name="n")
+    # strand_n = Strand(Sequence, name="")
+
+
+    #passing attribute dict to the Strand class' object constructor and outputting that to a strand dict
+    for strand, letter in zip(strands, domainsUsed):
+        attributeDict[strand] = letter 
+        name = letter 
+        strandDict[Strand(strand, name = name)] = 5e-6
+
+    t1 = Tube(strands = strandDict, complexes = SetSpec(max_size = len(strands)), name = "tube 1")
+
+    tube_result = tube_analysis(tubes = [t1], compute = ["pairs", "mfe"], model = thsModel)
+    
+    if(numMiRNA != 1):
+        sampled_structures = sample(strands = [strands[1], strands[0], strands[2]], num_sample = 5, model = thsModel)
+
 #Handles user input
 def start():
     print("Input the number of miRNA strands")
     numMiRNA = int(input())
-    i = 0
-    while(i < numMiRNA):
-        print("input the miRNA and its sequence")
+    if (numMiRNA == 1):
         miRNA = input()#User input must be in the same form as this: name,base sequence. Base sequence needs to be in all caps
         index = miRNA.split(",")
         if(checkCorrectCharacters(baseList, index[1])):
+            index[1] = index[1].replace(" ","")
             miRNADict.update({index[0]: index[1]})
-            i+=1
         else:
             print("Incorrect input")
-    andGateGen(miRNADict)
-start()
-
-unpairedStartLength = len(list(miRNADict.values())[0]) - round((len(list(miRNADict.values())[0])//3))
-
-#this was the annoying thing I was talking about ages ago where due to divisibility by 3 resulting in a number that may round up or down depending on the remainder. fixed by brute force
-if (unpairedStartLength % 3 == 2):
-    unpairedStartLength = unpairedStartLength + 1 
-elif (unpairedStartLength % 3 == 1):
-    unpairedStartLength = unpairedStartLength - 1
-else:
-    unpairedStartLength = unpairedStartLength
-
-#converting unpairedStart and End to DU+ notation and adding to the beginning and end of complexStructure respectively
-unpairedStart = "U" + str(unpairedStartLength)
-unpairedEnd = "U" + str(len(list(miRNADict.values())[-1]) - len(miRNA2))
-complexStructure[-1] = unpairedEnd
-complexStructure.insert(0, unpairedStart)
-
-#adds the base sequences of each miRNA and AND gate to strands to be used later
-for value in miRNADict.values():
-    strands.append(value)
-
-domainCodes = []
-
-#this writes the text to be put in the txt file for assigning domains to the DU+ structure, eg
-#domain a = AUCGACGACUGCCGUA
-#domain b = UUCGUAGCUAGCUGA etc...
-for strand, domain in zip(strands, domains):
-    domainCode = "domain " + domain + " = " + strand
-    domainCodes.append(domainCode)
-
-#this takes letters of the alphabet in order until there is 1 for each miRNA and AND gate
-domainsUsed = domains[0:len(domainCodes)]
-
-#the domainsUsed list is split into 2 halves, one with letters to be assigned to miRNAs and the other to AND gates
-mid = int((len(domainsUsed) + 1)/2)
-miRNAs = list(domainsUsed[mid-1:])
-andGates = list(domainsUsed[0:mid-1])
-
-seq.extend(miRNAs)
-
-integer = 0
-
-#reordering the miRNAs and AND gates to be in a pattern of: miRNA, AND gate, miRNA, AND gate, miRNA etc...
-while (integer < len(andGates)):
-    for domain in seq:
-        if((seq.index(domain)%2 ==0) and (seq.index(domain) + 1 < len(seq))):
-            seq.insert(seq.index(domain) + 1, andGates[integer])
-            integer += 1
-        else:
-            continue
-
-#writes all the calculated info to a text file
-outputFile.write(f'# miRNA complex structure in DU+ notation \nstructure complex = {"".join(complexStructure)}\n')
-outputFile.write(f'\n# base sequences for each RNA strand')
-for code in domainCodes:
-    outputFile.write(f"\n" + code)
-outputFile.write(f'\n \n# mapping RNA strands to the complex')
-outputFile.write(f'\ncomplex.seq = ')
-for sequence in seq:
-    outputFile.write(sequence + " ")
-
-#toehold switch stuff
-#assigning start and end of toeholdRegion
-toeholdStart = list(miRNADict.values())[0][0:unpairedStartLength]
-toeholdEnd = list(miRNADict.values())[-1][len(miRNA2):]
-
-toeholdAndGates = []
-
-#every strand but the first and last one which are already accounted for in toeholdStart and End
-middleStrands = strands[0:-1]
-del middleStrands[len(middleStrands)//2]
-
-#checks the rest of the strands to see if they are and gates
-for loopStrand in middleStrands:
-    count = 1
-    for char in range(0, len(loopStrand)):
-        if (loopStrand[char - 1] == loopStrand[char]):
-            count+=1
-            if (count == 6):
-                toeholdAndGates.append(count*loopStrand[char])
+        print(compStrand(list(miRNADict.values())[0]))
+    else:
+        i = 0
+        while(i < numMiRNA):
+            print("input the miRNA and its sequence")
+            miRNA = input()#User input must be in the same form as this: name,base sequence. Base sequence needs to be in all caps
+            index = miRNA.split(",")
+            if(checkCorrectCharacters(baseList, index[1])):
+                index[1] = index[1].replace(" ","")
+                miRNADict.update({index[0]: index[1]})
+                i+=1
             else:
-                continue
-        else:
-            count = 1
+                print("Incorrect input")
+        andGateGen(miRNADict)
+    unpairedParts(numMiRNA)
 
-del unpairedMiddleArray[0]
-del unpairedMiddleArray[-1]
- 
-integer = 0
+    #adds the base sequences of each miRNA and AND gate to strands to be used later
+    for value in miRNADict.values():
+        strands.append(value)
 
-#reordering sequences to form the toehold region
-while (integer < len(unpairedMiddleArray)):
-    for domain in toeholdAndGates:
-        if((toeholdAndGates.index(domain)%2 ==0) and (toeholdAndGates.index(domain) + 1 < len(toeholdAndGates))):
-            toeholdAndGates.insert(toeholdAndGates.index(domain) + 1, unpairedMiddleArray[integer])
-            integer += 1
-        else:
-            continue
+    domainOutputs()
 
-toeholdRegion = []
-
-#forming the toehold region
-toeholdRegion.insert(0, toeholdStart)
-toeholdRegion.extend(toeholdAndGates)
-toeholdRegion.append(toeholdEnd)
-toeholdRegion = "".join(str(i) for i in toeholdRegion)
-
-#this is the unpaired region of the toehold switch where the miRNA complex binds
-toehold = round((len(list(miRNADict.values())[0])//2))
-
-#the paired section of the toehold switch that unzips until the start codom
-thsDuplex = len(toeholdRegion) - toehold
-
-#the entire switch structure in DU+ including the hairpin loop
-thsStructure = ("U"+ str(toehold) + "D"+ str(thsDuplex) + "(U3D5(U15)U3)U21")
-
-#sequence of the toehold switch
-toeholdSwitchSequence = (compStrand(toeholdRegion) + "GGAUUUGCAAAAAAAAGAGGAGAGUAAAAUG" + reverse(toeholdRegion)[0:thsDuplex] + "AACCUGGCGGCAGCGCAAAAG")
-
-#writing the text file for ths
-outputFile2.write(f'# toehold switch structure in DU+ notation \n')
-outputFile2.write(f'structure toeholdSwitch = ' + thsStructure + '\n')
-outputFile2.write(f'\n# base sequences for each section of the toehold switch \n')
-outputFile2.write(f'domain a = ' + compStrand(toeholdRegion) + '\n')
-outputFile2.write(f'domain b = GGAUUUGCAAAAAAAAGAGGAGA N5 AUG \n')
-outputFile2.write(f'domain c = ' + reverse(toeholdRegion)[0:thsDuplex] + '\n')
-outputFile2.write(f'domain d = AACCUGGCGGCAGCGCAAAAG \n')
-outputFile2.write(f'# toeholdSwitch sequence = ' + toeholdSwitchSequence + '\n')
-outputFile2.write(f'\n# mapping RNA domains to the toehold switch \n')
-outputFile2.write(f'toeholdSwitch.seq = a b c d')
-
-#creating separate Strand object for toehold switch
-thsObject = Strand(toeholdSwitchSequence, domains[len(domainsUsed)])
-
-#attribute dict is a dictionary pairing each strand to a corresponding letter, these do not need to be the same letters used in the text files as these are for analysing the MFE of complexes
-attributeDict = {}
-
-#strand dict is the dictionary pairing each strand object to a corresponding concentration in M to then be passed onto a tube
-strandDict = {}
-
-#note to self:
-# strand_n = Strand("ACGUAGCUAGCUAGCAUC", name="n")
-# strand_n = Strand(Sequence, name="")
-
-
-#passing attribute dict to the Strand class' object constructor and outputting that to a strand dict
-for strand, letter in zip(strands, domainsUsed):
-    attributeDict[strand] = letter 
-    name = letter 
-    strandDict[Strand(strand, name = name)] = 5e-6
-
-t1 = Tube(strands = strandDict, complexes = SetSpec(max_size = len(strands)), name = "tube 1")
-
-tube_result = tube_analysis(tubes = [t1], compute = ["pairs", "mfe"], model = thsModel)
-
-sampled_structures = sample(strands = [strands[1], strands[0], strands[2]], num_sample = 5, model = thsModel)
+    toeholdSwitchGen(numMiRNA)
+    tubeDef(numMiRNA)
+start()
